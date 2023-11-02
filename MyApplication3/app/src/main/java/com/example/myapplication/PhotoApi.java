7a45a976c2e9d5d7aa73af9f4e51869e1403ca7e
thethanh23 <thanhctph19995@fpt.edu.vn>
2023-11-02 07:41:00 +0700

    lam them

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/PhotoApi.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PhotoApi {
+    @GET("photos") // Đặt URL API thực tế ở đây
+    Call<List<PhotoModel>> getPhotos();
+}
